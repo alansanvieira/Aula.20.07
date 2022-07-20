203b5b9093419b5fe09c7797113acea1390bc6f1
Alan Santana <alansantanavieira@gmail.com>
2022-07-20 16:47:33 -0300

    Adicionando informações no Postman

---
+++ PoCWebAPI/Models/Curso.cs
@@ -0,0 +1,11 @@
+﻿namespace PoCWebAPI.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public List<Aluno> Alunos { get; set; } = new List<Aluno>();
+    }
+}
---
+++ PoCWebAPI/Models/EscolaContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PoCWebAPI.Models;
+
+namespace PoCWebAPI.Models
+{
+    public class EscolaContext: DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder Modelagem)
+        {
+            Modelagem.Entity<Aluno>(Aluno =>
+            {
+                Aluno.HasKey(Coluna => Coluna.Id);
+            });
+
+            Modelagem.Entity<Curso>(Curso =>
+            {
+                Curso.HasKey(Coluna => Coluna.Id);
+
+                Curso.HasMany(Coluna => Coluna.Alunos);
+            });
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder Configuracao)
+        {
+            Configuracao.UseInMemoryDatabase("Escola");
+        }
+
+        public DbSet<PoCWebAPI.Models.Curso>? Curso { get; set; }
+    }
+}
